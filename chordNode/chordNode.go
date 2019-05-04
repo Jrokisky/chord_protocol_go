6643182810c00ad805108f21ea82e9b353077c12
Duncan Beard <duncanmbeard@gmail.com>
2019-05-04 18:07:04 -0400

    main now talks directly to node

--- chordNode/chordNode.go
+++ chordNode/chordNode.go
@@ -2,18 +2,59 @@ package chordNode
 
 import (
 	"fmt"
+
+	zmq "github.com/alecthomas/gozmq"
 )
 
+type fingerTableEntry struct {
+	Key       uint32
+	Successor uint32
+}
+type fingerTable struct {
+	Entries []fingerTableEntry
+	Size    int
+}
 type chordNode struct {
-	name string
+	ID        uint32
+	Successor uint32
+	Table     fingerTable
 }
 
-func New(name string) chordNode {
-	n := chordNode{name}
+func New(id uint32) chordNode {
+	n := chordNode{ID: id}
+	n.Table = fingerTable{Size: 32}
 	return n
 }
 
-func (n chordNode) HelloWorld() {
+func (n chordNode) Run() {
 
-	fmt.Printf("Hello from %s\n", n.name)
+	port := "5555"
+
+	context, _ := zmq.NewContext()
+	defer context.Close()
+
+	client, _ := context.NewSocket(zmq.REQ)
+	defer client.Close()
+
+	// TODO: Why is this localhost here and * on the server?
+	client.Connect("tcp://localhost:" + port)
+	// TODO: Why socket.connect vs socket.bind?
+	fmt.Printf("Client bound to port %s\n", port)
+
+	/*
+			for request in range(1, 10):
+		    print "Sending request ", request, "..."
+		    socket.send("Hello")
+		    message = socket.recv()
+		    print "Received reply", request, "[", message, "]"
+	*/
+
+	for i := 0; i < 10; i++ {
+		msg := fmt.Sprintf("Hello %d", i)
+		client.Send([]byte(msg), 0)
+		println("Sending", msg)
+
+		reply, _ := client.Recv(0)
+		println("Received", string(reply))
+	}
 }
--- main.go
+++ main.go
@@ -1,8 +1,61 @@
 package main
 
-import "chord/chordNode"
+import (
+	"chord/chordNode"
+	"fmt"
+	"math/rand"
+	"time"
+
+	zmq "github.com/alecthomas/gozmq"
+)
+
+type nodeAddress struct {
+	NodeID    uint32
+	IpAddress string
+	Port      string
+}
+
+type nodeDirectory struct {
+	nodes []nodeAddress
+}
 
 func main() {
-	node := chordNode.New("ABC")
-	node.HelloWorld()
+	// ringSize := 32 // 2^32
+
+	// User input loop, initially no nodes
+	nodes := make(map[uint32]nodeAddress)
+	// User adds node
+	newNode := chordNode.New(rand.Uint32())
+	newNodeAddress := nodeAddress{NodeID: newNode.ID, IpAddress: "localhost", Port: "5555"}
+	// Add node to directory
+	nodes[newNode.ID] = newNodeAddress
+	go newNode.Run()
+
+	port := "5555"
+
+	context, _ := zmq.NewContext()
+	defer context.Close()
+
+	server, _ := context.NewSocket(zmq.REP)
+	defer server.Close()
+
+	server.Bind("tcp://*:" + port)
+	fmt.Printf("Server bound to port %s\n", port)
+
+	// Server run loop
+	/*
+			message = socket.recv()
+		    print "Received request: ", message
+		    time.sleep(1)
+			socket.send("World from port %s" % port)
+	*/
+	for true {
+		msg, _ := server.Recv(0)
+		fmt.Printf("Received %s\n", msg)
+		time.Sleep(time.Second)
+		reply := fmt.Sprintf(" world")
+		server.Send([]byte(reply), 0)
+	}
+
+	time.Sleep(10000 * time.Millisecond)
 }
