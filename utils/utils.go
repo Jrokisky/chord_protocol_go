7beaa10e3c3bca95e46c049dc97bc37a710a4cd1
Justin Rokisky <jrokisky831@gmail.com>
2019-05-11 01:18:17 -0400

    Added hashing function

---
+++ utils/utils.go
@@ -0,0 +1,28 @@
+package utils
+
+import (
+    "crypto/sha1"
+    "math/big"
+    "encoding/binary"
+)
+
+func ComputeId(input string) uint32 {
+    // Hash input
+    hash := sha1.New()
+    hash.Write([]byte(input))
+    hashed_in := hash.Sum(nil)
+
+    // Conver input to int
+    hash_big := new(big.Int)
+    hash_big.SetBytes(hashed_in)
+
+    // Mod chord size
+    size_big := new(big.Int)
+    size_big.Exp(big.NewInt(2), big.NewInt(32), nil)
+    result_big := new(big.Int)
+    result_big = hash_big.Mod(hash_big, size_big)
+
+    // Convert to uint32
+    return binary.BigEndian.Uint32(result_big.Bytes())
+}
+
